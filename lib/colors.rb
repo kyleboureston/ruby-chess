7e247313f4be4f2e3779e8be0223b96457407518
Kyle Boureston <kyleboureston@gmail.com>
2022-11-24 14:15:20 -0600

    Refactor for rubocop

--- lib/colors.rb
+++ lib/colors.rb
@@ -9,7 +9,7 @@ class String
     def pink;           "\e[35m#{self}\e[0m" end
     def blue;           "\e[36m#{self}\e[0m" end
     def white;          "\e[37m#{self}\e[0m" end
-  
+
     def bg_black;       "\e[40m#{self}\e[0m" end
     def bg_red;         "\e[41m#{self}\e[0m" end
     def bg_green;       "\e[42m#{self}\e[0m" end
@@ -18,11 +18,10 @@ class String
     def bg_pink;        "\e[45m#{self}\e[0m" end
     def bg_blue;        "\e[46m#{self}\e[0m" end
     def bg_white;       "\e[47m#{self}\e[0m" end
-  
+
     def bold;           "\e[1m#{self}\e[22m" end
     def italic;         "\e[3m#{self}\e[23m" end
     def underline;      "\e[4m#{self}\e[24m" end
     def blink;          "\e[5m#{self}\e[25m" end
     def reverse_color;  "\e[7m#{self}\e[27m" end
-  end
-  
+end
