4acb3acf08da8ee62ab5c04e61eff277cb22a4bf
Kyle Boureston <kyleboureston@gmail.com>
2022-11-28 12:54:10 -0600

    Add initial logic to run game

---
+++ lib/main.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'pieces/piece'
+require_relative 'pieces/bishop'
+require_relative 'pieces/queen'
+require_relative 'pieces/rook'
+require_relative 'pieces/knight'
+require_relative 'pieces/king'
+require_relative 'pieces/pawn'
+require_relative 'display/board'
+require_relative 'display/messages'
+require_relative 'input'
+require_relative 'colors'
+require_relative 'board'
+require_relative 'player'
+require_relative 'game'
+
+Game.new.play
