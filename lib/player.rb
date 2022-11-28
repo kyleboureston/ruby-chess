9c4733e52f7456f3e90874b0a492b12932fdd370
Kyle Boureston <kyleboureston@gmail.com>
2022-11-28 12:52:14 -0600

    Add initial player methods

--- lib/player.rb
+++ lib/player.rb
@@ -2,8 +2,26 @@
 
 # Class for the different players
 class Player
-  def initialize(name, color)
+  attr_reader :name, :color
+
+  def initialize(name, color, board)
     @name  = name
     @color = color
+    @board = board
+  end
+
+  def find_pieces
+    response = []
+    @board.data.flatten.each do |piece|
+      next if piece.nil?
+
+      response << piece if piece.color == @color
+    end
+    response
+  end
+
+  def piece_positions
+    pieces = find_pieces
+    pieces.map(&:position)
   end
 end
