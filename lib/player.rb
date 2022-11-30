df29e26fc5510161e921630ae7640f995d952e06
Kyle Boureston <kyleboureston@gmail.com>
2022-11-30 09:45:20 -0600

    Add the players king to the Player instance variables

--- lib/player.rb
+++ lib/player.rb
@@ -2,12 +2,24 @@
 
 # Class for the different players
 class Player
+  attr_accessor :king
   attr_reader :name, :color
 
   def initialize(name, color, board)
     @name  = name
     @color = color
     @board = board
+    @king  = nil
+  end
+
+  def set_king
+    @king = @board.get_king(color)
+    set_king_on_pieces
+  end
+
+  def set_king_on_pieces
+    pieces = @board.find_friendly_pieces(@color)
+    pieces.each { |piece| piece.add_king(@king) }
   end
 
   def find_pieces
